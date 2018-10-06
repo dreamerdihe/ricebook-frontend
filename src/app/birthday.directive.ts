b237db7ff2a7a5e9658acdb92d65f31ce4bc1902
Dreamer_he <sysuheyuan@gmail.com>
2018-10-06 00:39:10 -0500

    Revise the bug in birthday validation

---
+++ src/app/birthday.directive.ts
@@ -0,0 +1,22 @@
+import { Input, Directive } from '@angular/core';
+import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
+
+
+
+@Directive({
+  selector: '[date]',
+  providers: [{provide: NG_VALIDATORS, useExisting: IsValidBirthDay, multi: true}]
+})
+export class IsValidBirthDay implements Validator {
+
+  validate(control: AbstractControl): {[key: string]: any} | null {
+    const now = Date.now();
+    const today = new Date(now);
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+    const restrict = new Date(year - 18, month, day);
+    const date = new Date(control.value);
+    return (date > restrict) ? {'date': {value: control.value}} : null;
+  }
+}
